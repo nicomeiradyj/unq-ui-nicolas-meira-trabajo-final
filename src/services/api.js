e2efaec4faf9183745a182f668af838dfc856517
Nicolas Meira <nicomeira05@gmail.com>
2026-06-29 17:36:02 -0300

    feature: Se implementa la funcion validate para validar la palabra tanto con la api y con los requerimientos.

--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,10 @@ export async function validateWord(word) {
     return data.exists;
 }
 
-function validate(word, usedWords, lastWord) {
+async function validate(word, usedWords, lastWord) {
     if (usedWords.includes(word)) return 'La palabra ya fue utilizada';
     if (lastWord && word[0] !== lastWord[lastWord.length - 1]) return 'No respeta la cadena';
-    if (!validateWord(word)) return 'La palabra no existe';
+    const exists = await validateWord(word);
+    if (!exists) return 'La palabra no existe';
     return null;
 }
